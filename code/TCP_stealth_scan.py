812b5755c870dc0549b42d2164e540bc06ee56cc
Hola <2721495886@qq.com>
2022-10-08 16:29:54 +0800

    finish

---
+++ code/TCP_stealth_scan.py
@@ -0,0 +1,20 @@
+from scapy.all import *
+
+
+def tcpstealthscan(dst_ip, dst_port, timeout=10):
+    pkts = sr1(IP(dst=dst_ip)/TCP(dport=dst_port, flags="S"), timeout=10)
+    if (pkts is None):
+        print("Filtered")
+    elif(pkts.haslayer(TCP)):
+        if(pkts.getlayer(TCP).flags == 0x12):
+            send_rst = sr(IP(dst=dst_ip) /
+                          TCP(dport=dst_port, flags="R"), timeout=10)
+            print("Open")
+        elif (pkts.getlayer(TCP).flags == 0x14):
+            print("Closed")
+        elif(pkts.haslayer(ICMP)):
+            if(int(pkts.getlayer(ICMP).type) == 3 and int(stealth_scan_resp.getlayer(ICMP).code) in [1, 2, 3, 9, 10, 13]):
+                print("Filtered")
+
+
+tcpstealthscan('172.16.111.121', 80)
